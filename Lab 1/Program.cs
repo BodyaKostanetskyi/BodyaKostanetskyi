3ca456a73f2ac6cc6f0a98ebadd5dd7ef5243471
Богдан <90748478+BodyaKostanetskyi@users.noreply.github.com>
2022-11-03 15:45:34 +0200

    Lb1

---
+++ Lab 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine("Задайте висоту в метрах");
+            Console.WriteLine("Задайте початкову швидкость в м/c");
+            double h;
+            double v;
+
+            h = Convert.ToDouble(Console.ReadLine());
+            v= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(h);
+            Console.WriteLine(v);
+            double t = (Math.Sqrt(2 * h * 10  + Math.Pow(v, 2)) - v) / 10;
+            Console.WriteLine(t + " с");   
+        }
+    }
+}
