aaac0d0c1031e7dd9003fa525e290e7f1f8b84c4
Богдан <90748478+BodyaKostanetskyi@users.noreply.github.com>
2022-11-03 15:46:54 +0200

    Lb2

---
+++ lab 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите чилсо k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите чилсо n");
+            double n = Convert.ToDouble(Console.ReadLine());
+            double a = (Math.Pow((-1), Math.Pow(k, 2) + k + 1) * Math.Pow(k, 2) / (2 * Math.Pow(k, 2) + 5));
+            if (0 <= n * n && n * n <= n * k)
+            {
+                Console.WriteLine(a);
+                
+            }
+            // else { Convert.ToInt32(Console.WriteLine((Math.Pow((-1), Math.Pow(k, 2) + k + 1) * Math.Pow(k, 2) / (2 * Math.Pow(k, 2) + 5))));
+            // }
+            else {
+                Console.WriteLine("Ты дурак");
+            }
+        }
+    }
+}
