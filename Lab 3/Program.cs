abe629c57a25b83265e919b8d3a4069e3ebcc0a6
Богдан <90748478+BodyaKostanetskyi@users.noreply.github.com>
2022-11-03 16:14:10 +0200

    Lab 3

---
+++ Lab 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+
+namespace lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite 4islo");
+            int k = Convert.ToInt32(Console.ReadLine());
+           
+            
+
+            if (k > 1000 & k < 9999)
+            {
+                if ((k / 1000 + (k / 100 % 10)) == (k%100/10+k%10))
+                {
+                    Console.WriteLine("krasava");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("nepravilno");                }
+
+                
+            }
+            else
+            {
+                Console.WriteLine("tu dyrak");
+            }
+        }
+    }
+}
